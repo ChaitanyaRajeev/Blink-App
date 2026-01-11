c3c874ddc3e3e6101c4b6d6f4d606fec05bd2d87
ChaitanyaRajeev <36008285+ChaitanyaRajeev@users.noreply.github.com>
2026-01-11 14:57:01 -0800

    Initial Commit

---
+++ Blink/BlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkApp.swift
+//  Blink
+//
+//  Created by Chaitanya Rajeev on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct BlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
